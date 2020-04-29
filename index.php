747c2e4ffbf59f1815c0a014e0e8b05ac891a8f9
kadayogit <guledodowaa@gmail.com>
2020-04-29 21:57:30 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>my website</title>
+</head>
+<body>
+<header style="background-color: green; text-align: center;">
+  <h2>Welcome to my website all people!</h2>
+</header>
+</body>
+</html>
