3ffd31bac24e5fe15fb3c546fbc4d928b5929671
kadayogit <guledodowaa@gmail.com>
2020-04-29 23:10:05 +0300

    added new features

--- index.php
+++ index.php
@@ -3,15 +3,7 @@
   <title>my website</title>
 </head>
 <body>
-<header style="background-color: green; text-align: center;">
-  <h2>Welcome to my website all people!</h2>
-  <?php  echo "Hello Php programming language!";?>
-  <p>i am changed the github website to my repository</p>
-  <form>
-    <input name="test" placeholder="Enter Name">
-    <input name="test" placeholder="Enter Name">
-    <input value="Login" type="submit">
-  </form>
-</header>
+ Pageak labaad:
+ <a href="magaca_pageka_aad_tagayso.html">Riix Halkan</a>
 </body>
 </html>
