803a9ad4d89b58323c3ee77eb2b2fc5f04e58ad8
kadayogit <guledodowaa@gmail.com>
2020-04-29 22:21:43 +0300

    added new features

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
   <h2>Welcome to my website all people!</h2>
   <?php  echo "Hello Php programming language!";?>
   <p>i am changed the github website to my repository</p>
+  <form>
+    <input name="test" placeholder="Enter Name">
+    <input name="test" placeholder="Enter Name">
+    <input value="Login" type="submit">
+  </form>
 </header>
 </body>
 </html>
